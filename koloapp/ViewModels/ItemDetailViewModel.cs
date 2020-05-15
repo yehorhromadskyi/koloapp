512f40587f4574630a19398ac50fce0ddb006725
Yehor Hromadskyi <yhromadskyi@exadel.com>
2020-05-15 23:00:27 +0300

    First commit

---
+++ koloapp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using koloapp.Models;
+
+namespace koloapp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
