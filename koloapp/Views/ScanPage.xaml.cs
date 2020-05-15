f60cd9f455bd56d580937baf9bca41b3667c76ea
Yehor Hromadskyi <yhromadskyi@exadel.com>
2020-05-15 23:42:48 +0300

    Add bar code scanning

--- koloapp/Views/ScanPage.xaml.cs
+++ koloapp/Views/ScanPage.xaml.cs
@@ -12,5 +12,13 @@ namespace koloapp.Views
         {
             InitializeComponent();
         }
+
+        void scanView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", "The barcode's text is " + result.Text + ". The barcode's format is " + result.BarcodeFormat, "OK");
+            });
+        }
     }
 }
